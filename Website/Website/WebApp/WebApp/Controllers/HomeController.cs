439d78b4e96b4481c714f630b024d306ba8c55ec
VoAnhHao_2001207055 <106418039+anhhao22121@users.noreply.github.com>
2023-11-30 00:27:34 +0700

    up

---
+++ Website/Website/WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+        private QuanLySuKienEntities db = new QuanLySuKienEntities(); // Tên của bạn có thể khác
+
+        public ActionResult Index()
+        {
+            var data = db.SuKiens.ToList();
+            return View(data);
+        }
+
+        public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
